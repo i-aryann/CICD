c808e1563d6e9981c7efa42955b88c4723bc2948
i-aryann <aryangupta.7263@gmail.com>
2025-09-02 00:16:25 +0530

    adding test cases

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,7 +1,11 @@
-from source.Calculator import add,subtract
+from source.Calculator import add,subtract,multiply
 
 def test_add():
     assert add(2,3)==5
     
 def test_sub():
     assert subtract(5,3)==2
+
+def test_multiply():
+    assert multiply(2,2)==4
+    assert multiply(1,1)==1
